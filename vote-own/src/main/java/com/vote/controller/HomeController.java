c1ae79585af650dd504b25fb08134b19fb8b479e
Weiyuhui <1264787656@qq.com>
2022-07-07 11:27:44 +0800

    比赛信息展示,以及时间修改展示

--- vote-own/src/main/java/com/vote/controller/HomeController.java
+++ vote-own/src/main/java/com/vote/controller/HomeController.java
@@ -1,9 +1,16 @@
 package com.vote.controller;
 
+import com.vote.common.config.RuoYiConfig;
 import com.vote.common.core.controller.BaseController;
+import com.vote.common.core.page.TableDataInfo;
+import com.vote.domain.Match;
+import com.vote.service.IMatchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * @author 魏渝辉
@@ -17,10 +24,17 @@ public class HomeController extends BaseController {
 
     private String prefix = "vote-own/";
 
+    @Autowired
+    private IMatchService matchService;
+
     @GetMapping("/system/home")
     public String home(ModelMap mmap)
     {
-//        mmap.put("version", RuoYiConfig.getVersion());
+        Match match = new Match();
+        List<Match> list = matchService.selectMatchList(match);
+
+        TableDataInfo dataTable = getDataTable(list);
+        mmap.put("dataTable", dataTable);
         System.out.println("home");
         return prefix + "UserHome";
     }
